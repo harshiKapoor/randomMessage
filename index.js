09ac67b188570a7c4fbd2d252d3759af534e2c2d
harshita.kapoor <harshita.kapoor@silverrailtech.com>
2021-08-06 13:27:39 +1000

    random message generator

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const messageCollection = ['Slow and steady wins the race','Be confident but never proud','Dont be in race'];
+
+const myMessage = () => {
+    return messageCollection[Math.floor(Math.random()*2)]
+}
+
+console.log(myMessage);
